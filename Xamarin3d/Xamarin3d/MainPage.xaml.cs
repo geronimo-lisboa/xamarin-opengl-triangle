771e9d446909a2e97b30834ab748bdcd50b49400
geronimo-lisboa <erikaeluciano2016@gmail.com>
2018-06-23 11:43:00 -0300

    OpenGLView sendo definida com xaml e renderizando algo.

--- Xamarin3d/Xamarin3d/MainPage.xaml.cs
+++ Xamarin3d/Xamarin3d/MainPage.xaml.cs
@@ -14,6 +14,22 @@ namespace Xamarin3d
 		public MainPage()
 		{
 			InitializeComponent();
+            openglView.HasRenderLoop = true;//Sem isso aqui ele não vai usar meu ponteiro de função.
+            openglView.OnDisplay = this.OpenGLViewOnDisplay;//Passa o ponteiro de função.
 		}
+
+        private float currentR = 0;
+        /// <summary>
+        /// Essa aqui é a função que trata da renderização da view de opengl;
+        /// </summary>
+        /// <param name="rect"></param>
+        private void OpenGLViewOnDisplay(Rectangle rect)
+        {
+            GL.ClearColor(currentR, 0, 0, 1.0f);
+            GL.Clear((ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit));
+            currentR += 0.01f;
+            if (currentR >= 1.0f)
+                currentR = 0;
+        }
 	}
 }
